8e366279ddaabe7e4419f93d7567a9361e0e3a2a
gsdutra <gabrielsprecherdutra@gmail.com>
2023-02-25 19:12:10 -0300

    feat: add urls/shorten route

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -1,6 +1,16 @@
+import { db } from "../database.connection.js"
+import { nanoid } from "nanoid"
+
 export async function postUrl(req, res){
 	try {
-		res.sendStatus(200)
+		const originalUrl = req.body.url
+		const shortUrl = nanoid(7)
+		const store = await db.query(`INSERT INTO urls ("userId", "originalUrl", "shortUrl") VALUES
+		('${res.locals.userId}','${originalUrl}','${shortUrl}')`)
+
+		const data = await db.query(`SELECT * FROM urls WHERE "shortUrl" = '${shortUrl}'`)
+
+		res.status(200).send({id:data.rows[0].id, shortUrl: shortUrl})
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
