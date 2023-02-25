731c720fedb42fbd89c59a2ba984b23a1bf27e45
gsdutra <gabrielsprecherdutra@gmail.com>
2023-02-25 18:04:37 -0300

    feat: complete signin route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import {db} from '../database.connection.js'
 import bcrypt from 'bcrypt'
+import {v4 as uuid} from 'uuid'
 
 export async function signup(req, res){
 	try {
@@ -14,7 +15,7 @@ export async function signup(req, res){
 		('${req.body.name}','${req.body.email}','${encryptedPassword}')
 		;`)
 		res.sendStatus(201)
-		
+
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
@@ -22,7 +23,21 @@ export async function signup(req, res){
 
 export async function signin(req, res){
 	try {
-		res.sendStatus(200)
+		const user = await db.query(`SELECT * FROM USERS WHERE email='${req.body.email}'`)
+
+		if (user.rows.length === 0)return res.sendStatus(401)
+
+		let token
+
+		if (bcrypt.compareSync(req.body.password, user.rows[0].password)){
+			token = uuid()
+			await db.query(`INSERT INTO sessions ("userId", "token") VALUES
+			('${user.rows[0].id}','${token}')`)
+		}else{
+			return res.sendStatus(401)
+		}
+
+		res.status(200).send({token: token})
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import {signin, signup} from '../controllers/authController.js'
 
-import { validateSignup } from "../middlewares/validadeSignup.js";
+import { validateSignup } from "../middlewares/validateSignup.js";
+import { validateSignin } from "../middlewares/validateSignin.js";
 
 const authRoutes = express.Router()
 
 authRoutes.post('/signup', validateSignup, signup)
-authRoutes.post('/signin/', signin)
+authRoutes.post('/signin/', validateSignin, signin)
 
 export default authRoutes
