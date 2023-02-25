23585a67e63aa7b83f78d42a55e09aa884063be7
gsdutra <gabrielsprecherdutra@gmail.com>
2023-02-25 16:09:36 -0300

    feat: complete signup route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,20 @@
 import {db} from '../database.connection.js'
+import bcrypt from 'bcrypt'
 
 export async function signup(req, res){
 	try {
-		const testQuery = await db.query(`SELECT * FROM users`)
-		res.status(200).send(testQuery.rows)
+		const emailAlreadyRegistered = await db.query(`SELECT * FROM users WHERE email='${req.body.email}'`)
+
+		if (emailAlreadyRegistered.rows.length > 0) return res.sendStatus(409)
+
+		const encryptedPassword = bcrypt.hashSync(req.body.password, 10)
+
+		const query = await db.query(`INSERT INTO users
+		(name, email, password) VALUES
+		('${req.body.name}','${req.body.email}','${encryptedPassword}')
+		;`)
+		res.sendStatus(201)
+		
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
