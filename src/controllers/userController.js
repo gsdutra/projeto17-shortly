2b2398fd84b9833491410896a71aec5afe266172
gsdutra <gabrielsprecherdutra@gmail.com>
2023-02-26 20:47:19 -0300

    feat: implement get users/me route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,24 @@
+import { db } from "../database.connection.js"
+
 export async function userMe(req, res){
 	try {
-		res.sendStatus(200)
+		const userId = res.locals.userId
+
+		const userData = await db.query(`SELECT * FROM users WHERE "id"='${userId}'`)
+
+		const name = userData.rows[0].name
+
+		const visitCount = await db.query(`SELECT SUM("visitCount") from urls WHERE "userId" = '${userId}'`)
+
+		const shortenedUrls = await db.query(`SELECT id, "shortUrl", "originalUrl" AS url, "visitCount" FROM
+		urls WHERE "userId" = '${userId}'`)
+
+		res.status(200).send({
+			id: userId,
+			name: name,
+			visitCount: visitCount.rows[0].sum,
+			shortenedUrls: shortenedUrls.rows
+		})
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,10 @@ import express from "express";
 
 import {userMe} from '../controllers/userController.js'
 
+import { verifyToken } from "../middlewares/verifyToken.js";
+
 const userRoutes = express.Router()
 
-userRoutes.get('/users/me', userMe)
+userRoutes.get('/users/me', verifyToken, userMe)
 
 export default userRoutes
