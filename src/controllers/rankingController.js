6af790b76a5b925b1184ee975570e0e7ed0f216b
gsdutra <gabrielsprecherdutra@gmail.com>
2023-02-27 16:47:37 -0300

    feat: add ranking route

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -1,6 +1,21 @@
+import { db } from "../database.connection.js"
+
 export async function ranking(req, res){
 	try {
-		res.sendStatus(200)
+		const ranking = await db.query(`
+			SELECT
+				users.id,
+				users.name,
+				COUNT(urls."userId") AS "linksCount",
+				SUM(urls."visitCount") AS "visitCount"
+			FROM urls
+			JOIN users ON
+				users.id = urls."userId"
+			GROUP BY users.id
+			ORDER BY "visitCount" DESC
+			LIMIT 10
+	;`)
+		res.status(200).send(ranking.rows)
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
